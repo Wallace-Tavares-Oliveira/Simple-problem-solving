4c2a41d1fc622f201b0ba2bbb6532b8a738aab7f
Wallace <wallacetavaresoliveira84@gmail.com>
2023-01-15 15:11:26 -0300

    attributes, methods and a constructor were created in the wheel class

--- BicycleRun/src/entities/Wheels.java
+++ BicycleRun/src/entities/Wheels.java
@@ -2,5 +2,15 @@ package entities;
 
 public class Wheels {
 
+	private boolean onRoad;
 	
+	// determines that the wheel is moving
+	public void toSpin() {
+		
+	}
+	
+	// determines that wheel is stopped
+	public void stop() {
+		
+	}
 }
