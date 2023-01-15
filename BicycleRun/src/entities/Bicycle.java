2b568759e2c0444d70b6f3aaaeeb965a0ac0bcb4
Wallace <wallacetavaresoliveira84@gmail.com>
2023-01-15 15:09:11 -0300

    attributes, methods and a constructor were created

--- BicycleRun/src/entities/Bicycle.java
+++ BicycleRun/src/entities/Bicycle.java
@@ -2,5 +2,26 @@ package entities;
 
 public class Bicycle {
 
+	// this is the base class
+		// the bicycle has two fields
+		public Wheels frontWheel;
+		public Wheels rearWheel;
+
+		// the Bicycle class has one constructor
+		public Bicycle(Wheels frontWheel, Wheels rearWheel) {
+			this.frontWheel = frontWheel;
+			this.rearWheel = rearWheel;
+		}
+		
+		// the Bicycle has two methods
+		// make the bike go
+		public void run() {
+			
+		}
+
+		// make the bike to stop
+		public void breaking() {
+
+		}
 	
 }
