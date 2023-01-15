28401ffa38c1c3769a00339cabb70cd7bf097baa
Wallace <wallacetavaresoliveira84@gmail.com>
2023-01-15 15:02:36 -0300

    Main, Bicyle and Wheel classes created

---
+++ BicycleRun/src/application/Main.java
@@ -0,0 +1,16 @@
+package application;
+
+import java.util.Scanner;
+
+import entities.Bicycle;
+import entities.Wheels;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+
+		sc.close();
+	}
+}
